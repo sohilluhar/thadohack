7f905dbbb19fe52b862d0df897c21bc1dffd0a25
sohilluhar <sohilayubluhar@gmail.com>
2020-03-13 15:49:22 +0530

    Init

---
+++ Scholar_Help/Common.py
@@ -0,0 +1,12 @@
+isLogin = False
+currentUser = None
+isTrustLogin = False
+trustkey = None
+trustVal = None
+forgotpassotp = None
+forgotpassotptime = None
+userphone = None
+encyptionkey = b'5mgdtkgjMcGj2hBGXAIqA7fcaEeS0_LAT4N8wRNF0ZA='
+isAdminLogin = False
+admin = None
+#comment
