67bf9fa797d49e5737eef07d9f1be6be7b74e354
teamzero-aaq <abdulquadir828@hmail.com>
2020-03-13 22:40:47 +0530

    User Profile Uploaded

--- Scholar_Help/views.py
+++ Scholar_Help/views.py
@@ -42,7 +42,7 @@ def login(request):
     return render(request, 'login.html', {})
 
 def test(request):
-    return render(request, 'doctument_page.html', {})
+    return render(request, 'profile_user.html', {})
 
 
 def adminlogin(request):
